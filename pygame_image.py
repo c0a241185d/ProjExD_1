01fc9b20456d9a8d4e938e724e9a968131e7e184
c0a241185d <c0a241185d@edu.teu.ac.jp>
2025-04-15 15:35:11 +0900

    練習問題6

--- pygame_image.py
+++ pygame_image.py
@@ -12,13 +12,16 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    bg_rct = bg_img.get_rect()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, bg_rct)
         screen.blit(kk_img, [300,200])
+        bg_rct.move_ip((-1, 0))
         pg.display.update()
         tmr += 1        
         clock.tick(200)
